6d3e2407c7c9e6e1987b00445c2b783927102863
Sajal Singh <as3199432@gmail.com>
2024-11-10 03:17:15 +0530

    footer animation

--- script.js
+++ script.js
@@ -77,13 +77,17 @@ tl.from("#hero1 , #page2",{
     opacity:0,
 },"-=1.2");
 }
+
+
+
 function cursurAnimation(){
     Shery.mouseFollower({
         skew:true,
         ease: "cubic-bezier(0.23, 1, 0.320, 1)",
         duration:.1,
     });
-Shery.makeMagnet("#nav-part2 h4");
+
+    Shery.makeMagnet("#nav-part2 h4");
 
 
 
@@ -138,31 +142,72 @@ function sheryAnimation(){
         gooey:true
     })
 }
+function FlagAnimation(){document.addEventListener("mousemove",
+    function(dets){
+        gsap.to("#flag",{
+            x:dets.x,
+            y:dets.y
+        })
+    })
+    document.querySelector("#hero5").addEventListener("mouseenter",
+        function(){
+            gsap.to("#flag",{
+                opacity : 1
+            })
+        })
+        
+    document.querySelector("#hero5").addEventListener("mouseleave",
+    function(){
+        gsap.to("#flag",{
+            opacity : 0
+        })
+    })
+}
+function footerAnimation() {
 
+    var clutter = ""
+    var clutter2 = ""
+    document.querySelector("#footer h1").textContent.split("").forEach(function (elem) {
+      clutter += `<span>${elem}</span>`
+    })
+    document.querySelector("#footer h1").innerHTML = clutter
+    document.querySelector("#footer h2").textContent.split("").forEach(function (elem) {
+      clutter2 += `<span>${elem}</span>`
+    })
+    document.querySelector("#footer h2").innerHTML = clutter2
+  
+  
+    document.querySelector("#footer-text").addEventListener("mouseenter", function () {
+      gsap.to("#footer h1 span", {
+        opacity: 0,
+        stagger: 0.05
+      })
+      gsap.to("#footer h2 span", {
+        delay: 0.35,
+        opacity: 1,
+        stagger: 0.1
+      })
+    })
+    document.querySelector("#footer-text").addEventListener("mouseleave", function () {
+      gsap.to("#footer h1 span", {
+        opacity: 1,
+        stagger: 0.1,
+        delay: 0.35,
+  
+      })
+      gsap.to("#footer h2 span", {
+        opacity: 0,
+        stagger: 0.05
+      })
+    })
+  }
+    
 
 loadingAnimation()
 cursurAnimation()
 locomativeAnimation()
 sheryAnimation()
+FlagAnimation()
+footerAnimation()
 
 
-document.addEventListener("mousemove",
-function(dets){
-    gsap.to("#flag",{
-        x:dets.x,
-        y:dets.y
-    })
-})
-document.querySelector("#hero5").addEventListener("mouseenter",
-    function(){
-        gsap.to("#flag",{
-            opacity : 1
-        })
-    })
-    
-document.querySelector("#hero5").addEventListener("mouseleave",
-function(){
-    gsap.to("#flag",{
-        opacity : 0
-    })
-})
